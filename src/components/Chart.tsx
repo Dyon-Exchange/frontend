c3f049da1efc8ed7a540c46a74814b15cf01fbb7
Conor <conorbrosnan10@gmail.com>
2021-07-21 10:29:26 +1000

    Make chart responsive

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 import { Text } from "@chakra-ui/react";
-import { LineChart, Line, XAxis, YAxis, Tooltip, Label } from "recharts";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Label,
+  ResponsiveContainer,
+} from "recharts";
 
 export default function Chart({ data }: any) {
   const CustomTooltip = ({ active, payload }: any) => {
@@ -23,43 +31,43 @@ export default function Chart({ data }: any) {
 
   return (
     <>
-      <LineChart
-        width={1000}
-        height={500}
-        data={data}
-        margin={{
-          top: 5,
-          right: 30,
-          left: 20,
-          bottom: 5,
-        }}
-      >
-        <XAxis
-          dataKey="time"
-          tickFormatter={(date: string, i: number) => {
-            if (i % 2 === 0) {
-              return new Date(date).toLocaleDateString("en", {
-                month: "numeric",
-                day: "numeric",
-              });
-            } else {
-              return "";
-            }
+      <ResponsiveContainer width="95%" height={400}>
+        <LineChart
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
           }}
-        ></XAxis>
-        <YAxis dataKey="price" tickFormatter={(price: any) => `$${price}`}>
-          <Label angle={270} position="left" style={{ textAnchor: "middle" }}>
-            Price
-          </Label>
-        </YAxis>
-        <Tooltip content={<CustomTooltip />} />
-        <Line
-          type="monotone"
-          dataKey="price"
-          stroke="#8884d8"
-          activeDot={{ r: 8 }}
-        />
-      </LineChart>
+        >
+          <XAxis
+            dataKey="time"
+            tickFormatter={(date: string, i: number) => {
+              if (i % 2 === 0) {
+                return new Date(date).toLocaleDateString("en", {
+                  month: "numeric",
+                  day: "numeric",
+                });
+              } else {
+                return "";
+              }
+            }}
+          ></XAxis>
+          <YAxis dataKey="price" tickFormatter={(price: any) => `$${price}`}>
+            <Label angle={270} position="left" style={{ textAnchor: "middle" }}>
+              Price
+            </Label>
+          </YAxis>
+          <Tooltip content={<CustomTooltip />} />
+          <Line
+            type="monotone"
+            dataKey="price"
+            stroke="#8884d8"
+            activeDot={{ r: 8 }}
+          />
+        </LineChart>
+      </ResponsiveContainer>
       <Text>Time</Text>
     </>
   );
