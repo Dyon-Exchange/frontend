8944b325747010dde3b0a34fcb774f6c79fa2f62
Aleksandar Gujas <alex.g@labrys.io>
2021-08-17 09:16:03 +1000

    pending orders page reformat

--- src/screens/PendingOrders.tsx
+++ src/screens/PendingOrders.tsx
@@ -9,26 +9,34 @@ import {
   Th,
   Td,
   chakra,
+  Container,
+  Spinner,
+  Flex,
 } from "@chakra-ui/react";
 import { useHistory, useLocation } from "react-router-dom";
-
-import { toCurrency } from "../formatting";
+import { IoMdCloseCircle } from "react-icons/io";
+import { toCurrency } from "../helpers/toCurrency";
 import orderApi from "../api/order";
 import { OrderSide, Asset, LimitOrder } from "../index.d";
 import { UserContext } from "../contexts/UserContext";
+import { formatDate } from "../helpers/formatDate";
 
-const TableRow = (props: { order: LimitOrder }) => {
+interface TableRowProps {
+  order: LimitOrder;
+}
+
+const TableRow: React.FC<TableRowProps> = ({ order }) => {
   const [loading, setLoading] = useState(false);
   const { allAssets, methods } = useContext(UserContext);
-
+  const { price, quantity, filled } = order;
   const asset: Asset = allAssets.filter(
-    (a) => props.order.productIdentifier === a.productIdentifier
+    (a) => order.productIdentifier === a.productIdentifier
   )[0];
 
   const cancelOrder = async () => {
     setLoading(true);
     try {
-      await orderApi.cancelOrder(props.order.orderId);
+      await orderApi.cancelOrder(order.orderId);
       methods.refreshUserOrders();
     } catch (e) {
       window.alert("There was an error canceling your order");
@@ -48,7 +56,16 @@ const TableRow = (props: { order: LimitOrder }) => {
         style={{ textAlign: "center", cursor: "pointer" }}
         onClick={handleRowClick}
       >
-        <chakra.img src={asset?.image} style={{ height: 50, width: 50 }} />
+        <chakra.img
+          src={asset?.image}
+          style={{
+            height: 32,
+            width: 32,
+            borderRadius: 16,
+            padding: 0,
+            margin: 0,
+          }}
+        />
       </Td>
       <Td
         style={{ cursor: "pointer", textAlign: "center", textJustify: "auto" }}
@@ -56,14 +73,23 @@ const TableRow = (props: { order: LimitOrder }) => {
       >
         {asset?.name} {asset?.year}
       </Td>
-      <Td> {props.order.filled.toFixed(2) || "-"}</Td>
-      <Td>{props.order.quantity || "-"}</Td>
-      <Td>{toCurrency(props.order.filledPriceAverage) || "-"}</Td>
-      <Td>{toCurrency(props.order.filledPriceTotal) || "-"}</Td>
+      <Td textAlign="center">
+        {asset ? formatDate(new Date(asset.createdAt)) : "-"}
+      </Td>
+      <Td textAlign="center">Limit Order</Td>{" "}
+      {/* All orders in this table are limit orders */}
+      <Td textAlign="center">{quantity ?? "-"}</Td>
+      <Td textAlign="center">{filled.toFixed(0) ?? "-"}</Td>
+      <Td textAlign="center">{toCurrency(price) ?? "-"}</Td>
+      <Td textAlign="center">{toCurrency(quantity * price)}</Td>
       <Td>
-        <Button onClick={cancelOrder} isLoading={loading}>
-          Cancel Order
-        </Button>
+        <Flex justifyContent="center">
+          {loading ? (
+            <Spinner />
+          ) : (
+            <IoMdCloseCircle size="20" onClick={cancelOrder} />
+          )}
+        </Flex>
       </Td>
     </Tr>
   );
@@ -92,65 +118,72 @@ const PendingOrders = () => {
     );
   }, [showOrderSide, userLimitOrders]);
 
-  return (
-    <VStack py="5%" width="100%">
-      <HStack width="100%" justifyContent="space-between" px="20%">
-        <Heading>Pending Orders</Heading>
-        <HStack>
-          <Button
-            bgColor={showOrderSide === "BID" ? "black" : undefined}
-            textColor={showOrderSide === "BID" ? "white" : undefined}
-            _hover={{
-              bgColor: "black",
-              textColor: "white",
-            }}
-            onClick={() => setShowOrderSide("BID")}
-          >
-            Buy Orders
-          </Button>
-          <Button
-            bgColor={showOrderSide === "ASK" ? "black" : undefined}
-            textColor={showOrderSide === "ASK" ? "white" : undefined}
-            _hover={{
-              bgColor: "black",
-              textColor: "white",
-            }}
-            onClick={() => setShowOrderSide("ASK")}
-          >
-            Sell Orders
-          </Button>
-        </HStack>
-      </HStack>
+  console.log("user limit orders:");
+  console.log(orders);
 
-      <Box py="5%">
-        {orders.length > 0 && (
-          <Table>
-            <Thead>
-              <Tr>
-                <Th></Th>
-                <Th></Th>
-                <Th>Filled</Th>
-                <Th>Quantity to {showOrderSide === "ASK" ? "sell" : "buy"}</Th>
-                <Th>Avg. Price</Th>
-                <Th>Order Total</Th>
-                <Th></Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              {orders.map((o, i) => (
-                <TableRow order={o} key={i} />
-              ))}
-            </Tbody>
-          </Table>
-        )}
-        {orders.length === 0 && (
-          <Heading size="md">
-            You have no pending {showOrderSide === "ASK" ? "sell" : "buy"}{" "}
-            orders
-          </Heading>
-        )}
-      </Box>
-    </VStack>
+  return (
+    <Container maxW="container.xl">
+      <VStack py="5%" width="100%">
+        <HStack width="100%" justifyContent="space-between" px="20%">
+          <Heading>Pending Orders</Heading>
+          <HStack>
+            <Button
+              bgColor={showOrderSide === "BID" ? "black" : undefined}
+              textColor={showOrderSide === "BID" ? "white" : undefined}
+              _hover={{
+                bgColor: "black",
+                textColor: "white",
+              }}
+              onClick={() => setShowOrderSide("BID")}
+            >
+              Buy Orders
+            </Button>
+            <Button
+              bgColor={showOrderSide === "ASK" ? "black" : undefined}
+              textColor={showOrderSide === "ASK" ? "white" : undefined}
+              _hover={{
+                bgColor: "black",
+                textColor: "white",
+              }}
+              onClick={() => setShowOrderSide("ASK")}
+            >
+              Sell Orders
+            </Button>
+          </HStack>
+        </HStack>
+
+        <Box py="5%">
+          {orders.length > 0 && (
+            <Table variant={"striped"} size={"sm"}>
+              <Thead>
+                <Tr>
+                  <Th></Th>
+                  <Th></Th>
+                  <Th textAlign="center">Date</Th>
+                  <Th textAlign="center">Type</Th>
+                  <Th textAlign="center">Volume</Th>
+                  <Th textAlign="center">Filled</Th>
+                  <Th textAlign="center">Price</Th>
+                  <Th textAlign="center">Order Total</Th>
+                  <Th textAlign="center">Cancel Order</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {orders.map((o, i) => (
+                  <TableRow order={o} key={i} />
+                ))}
+              </Tbody>
+            </Table>
+          )}
+          {orders.length === 0 && (
+            <Heading size="md">
+              You have no pending {showOrderSide === "ASK" ? "sell" : "buy"}{" "}
+              orders
+            </Heading>
+          )}
+        </Box>
+      </VStack>
+    </Container>
   );
 };
 
