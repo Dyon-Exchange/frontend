78c7c3a57e18393270aa8fd64fb023f99891221f
Aleksandar Gujas <alex.g@labrys.io>
2021-08-23 14:52:02 +1000

    update fe

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useMemo, useState } from "react";
+import React, { useContext, useMemo } from "react";
 import { Heading, Text } from "@chakra-ui/layout";
 import { filter } from "lodash";
 import {
